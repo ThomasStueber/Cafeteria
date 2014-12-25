e767447a9b481769eef003a5021b355aad0ff322
Yannick Runge <yannick.runge@student.uni-tuebingen.de>
2014-12-25 08:30:30 +0100

    Compiler bekommt jetzt eine Datei als Argument

--- Main.hs
+++ Main.hs
@@ -1,9 +1,12 @@
 module Main (main, parseJava) where
+import System.Environment
 import Scanner
 import Abs
 import Parser
 
-
-
 main = do
-  putStrLn (drawAst(parseJava(alexScanTokens "public class t {void test () { for(;;) {int a = 5;} }}")))
+  [arg] <- getArgs
+  src <- readFile arg
+  putStrLn (drawAst(parseJava(alexScanTokens src)))
+  -- putStrLn (drawAst(parseJava(alexScanTokens "public class t {void test () {
+  -- for(;;) {int a = 5;} }}")))
