84bab6ab3d199c6e4174fd76be83f7bbffe8fe2f
Yannick Runge <yannick.runge@student.uni-tuebingen.de>
2015-01-05 13:08:44 +0100

    added some examples to test the new lexer/parser features

--- Test.java
+++ Test.java
@@ -1,6 +1,15 @@
 public class Test {
+    /* multi line
+     * comment test */
     private int x;
+    // single line comment
     private int y;
+
+    /* Constructor */
+    public Test() {
+        x = 0;
+        y = 0;
+    }
 
     public void assign(int _x, int _y) {
         x = _x;
@@ -59,5 +68,33 @@ public class Test {
             y += (i + j) / 2;
             i++;
         }
+    }
+
+    public void unreachableCodeTest() {
+        int i = 2;
+        // return; // uncomment to test error message
+        i = 4;
+    }
+
+    public void switchTest() {
+        switch (x) {
+            case 0:
+                x++;
+                break;
+            case 1:
+                x--;
+                break;
+            default:
+                x = -1;
+                break;
+        }
+    }
+
+    public void testNumberFormats() {
+        int binary = 0b01011;
+        int octal = 012;
+        int hex = 0x14;
+        // double d = 3.4d;
+        // float f = 4.1f;
     }
 }
