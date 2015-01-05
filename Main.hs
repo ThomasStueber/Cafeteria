abd763b7cdf86c05cd3e00e087c1652a8e1e2ea2
Yannick Runge <yannick.runge@student.uni-tuebingen.de>
2015-01-05 13:09:56 +0100

    Altered Main.hs: Start Parser now with ./Main <MyFile.java>

--- Main.hs
+++ Main.hs
@@ -1,9 +1,11 @@
 module Main (main) where
+import System.Environment
 import Scanner
 import Abs
 import Parser
 
-
-
 main = do
-  putStrLn (drawAst (parseJava(alexScanTokens "public class t {void test () {a = 0x123; } }")))
+  [arg] <- getArgs
+  src <- readFile arg
+  putStrLn (drawAst(parseJava(alexScanTokens src)))
+  -- putStrLn (drawAst (parseJava(alexScanTokens "public class t {void test () {a = 0x123; } }")))
