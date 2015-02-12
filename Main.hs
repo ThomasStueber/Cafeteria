00da1f18e534e49ae6652e85110d90893640e153
Yannick Runge <yannick.runge@student.uni-tuebingen.de>
2015-02-12 18:19:15 +0100

    renamed files and extended Main.hs

--- Main.hs
+++ Main.hs
@@ -4,10 +4,10 @@ import Scanner
 import Abs
 import Parser
 import Types
+import Translation
 
 main = do
   [arg] <- getArgs
   src <- readFile arg
-  --putStrLn (drawAst(parseJava(alexScanTokens src)))
-  putStrLn (show (typeCheck (parseJava(alexScanTokens src))))
+  putStrLn (show (ap_getCode(translateC(head(typeCheck (parseJava(alexScanTokens src)))))))
   -- putStrLn (drawAst (parseJava(alexScanTokens "public class t {void test () {a = 0x123; } }")))
